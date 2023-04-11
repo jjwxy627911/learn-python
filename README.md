ac22080d807489615b9d10cad42222dcff32020a
蒋经伟 <jingwei.jiang@mihoyo.com>
2023-04-11 19:50:44 +0800

    修改readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # learn-python
+
+## 参考文档
+https://python3-cookbook.readthedocs.io/zh_CN/latest/preface.html
---
+++ python-cookbook/1-数据结构和算法/1.1 将序列分解为单独的变量.py
@@ -0,0 +1,10 @@
+p = (4, 5)
+x, y = p
+print(x, y)
+
+
+data = ["ACME", 50, 91.1, (2012, 12, 21)]
+
+name, shares, price, date = data
+
+print(date)
